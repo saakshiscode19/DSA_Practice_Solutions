2e8d268a68ac552f9e9c66e0f8ac8b4542b70b14
Saakshi Sharma <12345678+saakshiscode19@users.noreply.github.com>
2025-12-17 17:14:41 +0530

    new questions

---
+++ MaxAreaHistogram.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+
+public class MaxAreaHistogram {
+    public static void maxArea(int arr[]){
+        int nsr[] = new int[arr.length];
+       int nsl[] = new int[arr.length]; 
+
+       //next smaller right
+        Stack<Integer> s = new Stack<>();
+        
+
+    }
+}
